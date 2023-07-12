2d2c67b335cd5393da7bf015ecc661cb1871c60b
Hermit241 <81275243+Hermit241@users.noreply.github.com>
2023-07-13 07:59:55 +0800

    Added Model Binding

--- project/src/main/java/my/edu/utem/ftmk/dad/project/controller/AttendanceMenuController.java
+++ project/src/main/java/my/edu/utem/ftmk/dad/project/controller/AttendanceMenuController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import my.edu.utem.ftmk.dad.project.model.Attendance;
@@ -17,15 +17,15 @@ import my.edu.utem.ftmk.dad.project.model.Student;
 
 @Controller
 public class AttendanceMenuController {
-//test
-	// changed something again
+
 	@GetMapping("/attendance/list")
 	public String getAttendance(Model model){
 		
 		String uri = "http://localhost:8080/attendanceapp/api/attendances";
 		
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Attendance[]> response = restTemplate.getForEntity(uri, Attendance[].class);
+		ResponseEntity<Attendance[]> response =
+				restTemplate.getForEntity(uri, Attendance[].class);
 		
 		Attendance attendances[] = response.getBody();
 		
@@ -39,15 +39,23 @@ public class AttendanceMenuController {
 	@GetMapping("/attendance/matriccardreader")
 	public String displayrMatricCardReader(Model model){
 		
+		Attendance attendance = new Attendance();
+		attendance.setAttendanceInputType("matric card reader");
+		
 		model.addAttribute("examinations", getExaminations());
+		model.addAttribute("attendance", attendance);
 		
 		return "matriccardreader";
 	}
 	
 	@GetMapping("/attendance/fingerprintreader")
 	public String displayFingerprintReader(Model model){
+
+		Attendance attendance = new Attendance();
+		attendance.setAttendanceInputType("fingerprint reader");
 		
 		model.addAttribute("examinations", getExaminations());
+		model.addAttribute("attendance", attendance);
 		
 		return "fingerprintreader";
 	}
@@ -56,13 +64,18 @@ public class AttendanceMenuController {
 		String uri = "http://localhost:8080/attendanceapp/api/examinations";
 		
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Examination[]> response = restTemplate.getForEntity(uri, Examination[].class);
+		ResponseEntity<Examination[]> response =
+				restTemplate.getForEntity(uri, Examination[].class);
 		
 		Examination examinations[] = response.getBody();
 		
 		return Arrays.asList(examinations);
 	}
 	
+	/*
+	 * A relic of bad code
+	 * 
+	 * 
 	@RequestMapping("/attendance/save/matricno/{matricno}/{exam}")
 	public String getStudentFromMatricNo(@RequestParam String matricno, @RequestParam String exam, Model model) {
 		String studentUri = "http://localhost:8080/attendanceapp/api/students/matricNo/" + matricno;
@@ -114,38 +127,95 @@ public class AttendanceMenuController {
 		
 		return "recordedattendance";
 	}
+	*/
 	
-	public Attendance saveAttendance(Student student, Examination examination, String inputType) {
+	/**
+	 * 
+	 * Processes the Post request from the frontend to save the attendance of the 
+	 * student.
+	 * 
+	 * @param attendance ModelAttribute of the attendance Object
+	 * @param model Model object to send to the "recorded attendance" view
+	 * @return 
+	 * If the provided matricno or fingerprint id does not belong to any student
+	 * in the database, it will redirect back to the last html view.
+	 * Otherwise, it will redirect to the "recorded attendance" view.
+	 * 
+	 */
+	
+	@PostMapping("/attendance/save")
+	public String saveAttendance(@ModelAttribute("attendance") Attendance attendance, Model model) {
 		
 		String attendanceUri = "http://localhost:8080/attendanceapp/api/attendances";
+		String examUri = "http://localhost:8080/attendanceapp/api/examinations/"
+		+ attendance.getExamination().getId();
+		String studentUri = "";
+		String returnPath = "";
 		
+		if (attendance.getAttendanceInputType().
+				compareTo("matric card reader") == 0) {
+			studentUri = "http://localhost:8080/attendanceapp/api/students/matricNo/" 
+				+ attendance.getStudent().getMatricno();
+			
+			returnPath = "redirect:/attendance/matriccardreader";
+		}
+		else if (attendance.getAttendanceInputType().
+				compareTo("fingerprint reader") == 0) {
+			studentUri = "http://localhost:8080/attendanceapp/api/students/matricNo/" 
+					+ attendance.getStudent().getFingerprintid();
+			returnPath = "redirect:/attendance/fingerprintreader";
+		}
+			
 		RestTemplate restTemplate = new RestTemplate();
 		
-		Attendance attendance = new Attendance();
-		attendance.setExamination(examination);
+		ResponseEntity<Student> studentResponse =
+				restTemplate.getForEntity(studentUri, Student.class);
+		
+		if (!studentResponse.hasBody()) {
+			System.out.println("No such student.");
+			return returnPath;
+		}
+		
+
+		ResponseEntity<Examination> examResponse = 
+				restTemplate.getForEntity(examUri, Examination.class);
+		
+		Student student = studentResponse.getBody();
+		Examination examination = examResponse.getBody();
 		attendance.setStudent(student);
-		attendance.setAttendanceInputType(inputType);
+		attendance.setExamination(examination);
 		
 		ResponseEntity<Attendance> attendanceResponse = 
-				restTemplate.postForEntity(attendanceUri, attendance, Attendance.class);
+				restTemplate.postForEntity(
+						attendanceUri, attendance, Attendance.class);
 		attendance = attendanceResponse.getBody();
-		attendance.setAttendancestatus(validateAttendance(examination, attendance));
+		attendance.setAttendancestatus(
+				validateAttendance(attendance.getExamination(), attendance));
 		restTemplate.put(attendanceUri, attendance, Attendance.class);
 		
 		System.out.println(attendance.getAttendanceInputType());
 		System.out.println(attendanceResponse);
 
-		return attendance;
+		model.addAttribute("attendance", attendance); 
+
+		return "recordedattendance";
 	}
 	
 	private String validateAttendance(Examination exam, Attendance attendance) {
 		String attendanceStatus = "On-Time";
 
-		long timeDiff =attendance.getAttendtime().getTime() - exam.getExamtime().getTime();
+		long timeDiff = attendance.getAttendtime().getTime() 
+				- exam.getExamtime().getTime();
 		long lateTime = 10 * 60 * 1000;
+		
+		/* 
+		 * Debugging print command, can be ignored
+		 * 
 		System.out.println(attendance.getAttendtime().getTime() +"|" + attendance.getAttendtime() 
 		+ " - " + exam.getExamtime().getTime() + "|" + exam.getExamtime());
 		System.out.println(timeDiff + " " + lateTime);
+		*/
+		
 		if (timeDiff > lateTime) {
 			attendanceStatus = "Late";
 		}
